4cd69110ca26e0baba466c3d786fba8392746a06
Mr Jensen <Colts45@Mrs-MacBook-Pro.local>
2021-10-14 21:46:36 -0400

    10/14/21 9:45 pm

--- MSPB.R
+++ MSPB.R
@@ -1,3 +1,14 @@
-setwd("~/Downloads/SECoPA workshop")
+setwd("~/Downloads/r-git/myrepo")
 
+library(gitcreds)
+
+gitcreds::gitcreds_set()
+
+library(credentials)
+
+set_github_pat()
+
+#Merit Service Protection Board - Path 1 survey
 d <- MSPB_p1
+
+head(d)
